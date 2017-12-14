f6401458028a3d9f487fb28de6b418d763fbd288
d.kopysov <kopysovdenisalexeevich@gmail.com>
2017-12-14 18:25:56 +0400

    v0.1.1
    CreateGraphics

--- app.py
+++ app.py
@@ -34,7 +34,14 @@ def clearBDAs():
 
 @app.route("/graphics", methods=['GET'])
 def graphic():
-    return render_template('Graphics.html')
+    legend1 = 'Temperatures'
+    temperatures1 = [73.7, 73.4, 73.8, 72.8, 68.7, 65.2,
+                    61.8, 58.7, 58.2, 58.3, 60.5, 65.7,
+                    70.2, 71.4, 71.2, 70.9, 71.3, 71.1]
+    times1 = ['12:00PM', '12:10PM', '12:20PM', '12:30PM', '12:40PM', '12:50PM',
+             '1:00PM', '1:10PM', '1:20PM', '1:30PM', '1:40PM', '1:50PM',
+             '2:00PM', '2:10PM', '2:20PM', '2:30PM', '2:40PM', '2:50PM']
+    return render_template('Graphics.html',values1=temperatures1, labels1=times1, legend1=legend1)
 
 @app.route("/graphics", methods=['POST'])
 def graphics():
