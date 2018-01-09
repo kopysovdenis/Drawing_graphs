11173d9a8667db61c22120b6a4029e981e20d817
d.kopysov <kopysovdenisalexeevich@gmail.com>
2018-01-09 10:30:30 +0400

    review
    form review

--- app.py
+++ app.py
@@ -21,28 +21,20 @@ namesFolders = ['BCO_UNION_NO_Accept',
                 ]
 filescount = [2.0, 1.0, 1.0]
 
-start_intense = 1140 #реестров в час, стартовая интенсивность
-final_intense = 1140 #реестров в час, финишная интенсивность
-step_length = 600 #минут, длительность "полки"
-interval = 5         #минут, частота отправки файлов
-duration = 600       #минут, длительность теста
-init_delay = 0
-step_length1 = 5   #(interval = 5 :раз в 5 минут)
-files_count = 2
+# start_intense = 1140 #реестров в час, стартовая интенсивность
+# final_intense = 1140 #реестров в час, финишная интенсивность
+# step_length = 600 #минут, длительность "полки"
+# interval = 5         #минут, частота отправки файлов
+# duration = 600       #минут, длительность теста
+# init_delay = 0
+# step_length1 = 5   #(interval = 5 :раз в 5 минут)
+# files_count = 2
 
 smit = sqlQuery()
 graf = GraphicUtilCPU()
 param_stab = param_stabs()
 
-if sum(percent) == 100:
-    for n, nf, p, fc in zip(names, namesFolders, percent, filescount):
-        out = param_stab.make_intervals(start_intense * p / 100, final_intense * p / 100, interval, duration,
-                                        step_length, step_length1, files_count)
-        out = [out[0]] + [[i[0], i[1] / fc] for i in out[1:]]
-        resu.append(out)
-    print(resu[0][1:])
-    print(resu[1][1:])
-    print(resu[2][1:])
+
 
 app = Flask(__name__)
 
@@ -112,6 +104,9 @@ def graphics():
        temperatures3 = VarCh1['Enroll']
        legend4 = 'Enrolled'
        temperatures4 = VarCh1['Enrolled']
+       legend51 = 'Enrolled(Усред.до 10 минут)'
+# Не хватает среднего времени обработки реестров
+
 # --2 График------------------------------------------------------------------------------------------------------------
        legend5 = 'New'
        temperatures5 = Newall
@@ -129,21 +124,30 @@ def graphics():
        Enrolled10 = smit.getEnrolled10(filess)
        LabelsWeb = 'Успешные web-операции'
        FainWebs = smit.dataProcessing(request.form['ValuesTest'])
-       """start_intense = request.form['start_intense']
-#final_intense = request.form['final_intense']
-#interval = request.form['interval']
-#duration = request.form['duration']
-#step_length = request.form['step_length']
-#step_length1 = request.form['step_length1']
-#files_count = 2"""
+       start_intense = int(request.form['start_intense'])
+       final_intense = int(request.form['final_intense'])
+       interval = int(request.form['interval'])
+       duration = int(request.form['duration'])
+       step_length = int(request.form['step_length'])
+       step_length1 = int(request.form['step_length1'])
+       files_count = 2
 
-
+       if sum(percent) == 100 and start_intense >= 0 and final_intense >= 0 and interval >= 0 and duration >= 0 and step_length >= 0:
+           for n, nf, p, fc in zip(names, namesFolders, percent, filescount):
+               out = param_stab.make_intervals(start_intense * p / 100, final_intense * p / 100, interval, duration,
+                                               step_length, step_length1, files_count)
+               out = [out[0]] + [[i[0], i[1] / fc] for i in out[1:]]
+               resu.append(out)
+           print(resu[0][1:])
+           print(resu[1][1:])
+           print(resu[2][1:])
 
        return render_template('TestGrapics.html', labels1=times,
                               NewLegend=legend1, NewValues=temperatures1,
                               CheckLegend=legend2, CheckValues=temperatures2,
                               EnrollLegend=legend3, EnrollValues=temperatures3,
                               EnrolledLegend=legend4, EnrolledValues=temperatures4,
+                              legend51 = legend51,
                               Legend1=legend5, Values1=temperatures5,
                               Legend2=legend6, Values2=temperatures6,
                               Legend3=legend7, Values3=temperatures7,
